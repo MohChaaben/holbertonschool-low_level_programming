9f24a699e523b1b9806439c55405fdaa267f524b
MohChaaben <mohamedchaaben21@gmail.com>
2020-02-07 21:36:09 +0000

    5th task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * execpt q & e
+ *Return: Always 0
+ */
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != 'q' && i != 'e')
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
+}
