8bb0d59d4df86000c912b5b6f1c212928ba8d196
MohChaaben <mohamedchaaben21@gmail.com>
2020-02-07 22:45:21 +0000

    program that prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print the alpabet in lower case
+ * @void: No parameters
+ * Return: 0
+ */
+int main(void)
+{
+char i;
+for (i = 'z' ; c >= 'a' ; c--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
