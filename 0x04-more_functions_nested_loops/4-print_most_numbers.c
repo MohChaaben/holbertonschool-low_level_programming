8ac6a0aebb9fd26eadd13ee06f14b73ec795f665
MohChaaben <mohamedchaaben21@gmail.com>
2020-02-13 12:30:14 +0000

    5th task code fixed

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,10 @@
-#include "holberton.h";
+#include "holberton.h"
 /**
  * print_most_numbers - same with pint-numbers but don't print 2 and 4.
  * @void
  * Return: void.
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 int i;
 for (i = 0; i <= 9; i++)
@@ -14,5 +14,5 @@ if (i != 2 && i != 4)
 _putchar (i + '0');
 }
 }
-_putchar ("\n");
+_putchar ('\n');
 }
