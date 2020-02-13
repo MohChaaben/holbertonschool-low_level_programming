aae956b8fc4e121bd9c3afb5f291a331739352c8
MohChaaben <mohamedchaaben21@gmail.com>
2020-02-13 10:28:31 +0000

    same function as Task 3 but do not print 2 & 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,18 @@
 #include "holberton.h";
 /**
- * print-numbers - a function that prints the numbers, from 0 to 9.
+ * print_most_numbers - same with pint-numbers but don't print 2 and 4.
  * @void
  * Return: void.
  */
-void print_numbers(void);
+void print_most_numbers(void);
 {
 int i;
 for (i = 0; i <= 9; i++)
 {
+if (i != 2 && i != 4)
+{
 _putchar (i + '0');
+}
 }
 _putchar ("\n");
 }
