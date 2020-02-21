44c20ed3d904bca9ed0db5c67979f1e39259c2c4
MohChaaben <mohamedchaaben21@gmail.com>
2020-02-21 09:11:33 +0000

    firs function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _strcat - a function that concatenates two strings.
+ * @dest: the string to concatenate on.
+ * @src: the string to concatenate.
+ * Return: dest.
+*/
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+for (i = 0; dest[i]; i++)
+;
+for (j = 0; src[j]; j++)
+{
+dest[i] = src[j];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
