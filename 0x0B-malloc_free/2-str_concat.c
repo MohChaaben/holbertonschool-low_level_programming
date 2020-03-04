ae31e5efd9130f737806f9c6ea63279c4eab13f1
MohChaaben <mohamedchaaben21@gmail.com>
2020-03-04 11:25:48 +0000

    code fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,9 @@ char *str_concat(char *s1, char *s2)
 int i, j, a, b;
 char *p;
 if (s1 == NULL)
-*s1 = '\0';
+s1 = "";
 if (s2 == NULL)
-*s2 = '\0';
+s2 = "";
 for (i = 0; s1[i]; i++)
 ;
 for (j = 0; s2[j]; j++)
